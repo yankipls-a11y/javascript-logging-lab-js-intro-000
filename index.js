3bd6e30929f3010595dedae75dc03abd45b85c2c
Aiyanna Tiangco <freedolphin.haru@gmail.com>
2020-01-08 07:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ console.error("Hey man we're calling an error!")
 console.log("Hello is it me you're looking for?")
 
 console.warn('hey dude that might be a bad idea...') //supposedly warns the user for bad actions
-
